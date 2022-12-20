b17483b649ee85c281cdd112ca307081c31226ec
Louis Wilbrink <louis.wilbrink@gmail.com>
2022-12-20 09:24:11 -0500

    * Refactor minigrep app to use a library.

--- rust-doc-programs/ch_12_minigrep/target/debug/ch_11_minigrep.d
+++ rust-doc-programs/ch_12_minigrep/target/debug/ch_11_minigrep.d
@@ -1,1 +1,1 @@
-/Users/louiswilbrink/Projects/learn-rust/rust-doc-programs/ch_12_minigrep/target/debug/ch_11_minigrep: /Users/louiswilbrink/Projects/learn-rust/rust-doc-programs/ch_12_minigrep/src/main.rs
+/Users/louiswilbrink/Projects/learn-rust/rust-doc-programs/ch_12_minigrep/target/debug/ch_11_minigrep: /Users/louiswilbrink/Projects/learn-rust/rust-doc-programs/ch_12_minigrep/src/lib.rs /Users/louiswilbrink/Projects/learn-rust/rust-doc-programs/ch_12_minigrep/src/main.rs
